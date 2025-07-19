c16915bbcfe9a0560759e1b95a291163aaf9f6f7
kishore1307 <67825033+kishore1307@users.noreply.github.com>
2025-07-19 21:37:04 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -47,7 +47,7 @@ resource "aws_route_table" "public" {
 
 # Route Table Association
 resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public_a.id
+  subnet_id      = aws_subnet.public_b.id
   route_table_id = aws_route_table.public.id
 }
 
@@ -81,7 +81,7 @@ resource "aws_security_group" "web_sg" {
 resource "aws_instance" "web" {
   ami                         = "ami-0c94855ba95c71c99"
   instance_type               = "t2.micro"
-  subnet_id                   = aws_subnet.public_a.id
+  subnet_id                   = aws_subnet.public_b.id
   vpc_security_group_ids      = [aws_security_group.web_sg.id]
   associate_public_ip_address = true
 
