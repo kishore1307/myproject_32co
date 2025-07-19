a774da0b89477c5ce70d7e389369a30ac0082f3d
kishore1307 <67825033+kishore1307@users.noreply.github.com>
2025-07-19 19:35:34 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -59,7 +59,7 @@ resource "aws_security_group" "web_sg" {
 resource "aws_instance" "web" {
   ami                         = "ami-0c02fb55956c7d316"
   instance_type               = "t2.micro"
-  subnet_id                   = module.vpc.public_subnet_id
+  subnet_id                   = aws_subnet.public.id
   vpc_security_group_ids      = [aws_security_group.web_sg.id]
   associate_public_ip_address = true
 
