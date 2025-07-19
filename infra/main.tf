0805ef03c31e0b93d282fed90ac985a6b6abb9c1
kishore1307 <67825033+kishore1307@users.noreply.github.com>
2025-07-19 21:38:41 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -6,20 +6,39 @@ resource "random_id" "bucket_id" {
   byte_length = 4
 }
 
+# VPC
 resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
+
   tags = {
     Name = "main-vpc"
   }
 }
 
+# Subnet A (us-east-1a)
+resource "aws_subnet" "public_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-a"
+  }
+}
+
+# Subnet B (us-east-1b)
 resource "aws_subnet" "public_b" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-b"
+  }
 }
 
 # Internet Gateway
@@ -45,12 +64,19 @@ resource "aws_route_table" "public" {
   }
 }
 
-# Route Table Association
-resource "aws_route_table_association" "public" {
+# Associate Subnet A to Route Table
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public.id
+}
+
+# Associate Subnet B to Route Table
+resource "aws_route_table_association" "public_b" {
   subnet_id      = aws_subnet.public_b.id
   route_table_id = aws_route_table.public.id
 }
 
+# Security Group
 resource "aws_security_group" "web_sg" {
   name        = "web-sg"
   description = "Allow HTTP and SSH"
@@ -78,10 +104,11 @@ resource "aws_security_group" "web_sg" {
   }
 }
 
+# EC2 Instance
 resource "aws_instance" "web" {
   ami                         = "ami-0c94855ba95c71c99"
   instance_type               = "t2.micro"
-  subnet_id                   = aws_subnet.public_b.id
+  subnet_id                   = aws_subnet.public_a.id
   vpc_security_group_ids      = [aws_security_group.web_sg.id]
   associate_public_ip_address = true
 
@@ -90,6 +117,7 @@ resource "aws_instance" "web" {
   }
 }
 
+# RDS Subnet Group
 resource "aws_db_subnet_group" "default" {
   name       = "main-db-subnet-group"
   subnet_ids = [
@@ -102,6 +130,7 @@ resource "aws_db_subnet_group" "default" {
   }
 }
 
+# RDS Instance
 resource "aws_db_instance" "rds" {
   allocated_storage      = 20
   engine                 = "mysql"
@@ -116,6 +145,7 @@ resource "aws_db_instance" "rds" {
   db_subnet_group_name   = aws_db_subnet_group.default.name
 }
 
+# S3 Bucket
 resource "aws_s3_bucket" "static_assets" {
   bucket = "devops-home-assessment-static-assets-${random_id.bucket_id.hex}"
 
