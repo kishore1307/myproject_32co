211f084a5d3cb90495f7324fc38d91c0649916ba
kishore1307 <67825033+kishore1307@users.noreply.github.com>
2025-07-19 21:35:06 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -15,7 +15,7 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_subnet" "public_a" {
+resource "aws_subnet" "public_b" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "us-east-1b"
