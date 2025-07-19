2fa2b632cf793d3a50fcb79fc09d0a353e8948a7
EC2 Default User <ec2-user@ip-172-31-81-14.ec2.internal>
2025-07-19 13:51:16 +0000

    added files

--- infra/s3.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "static_assets" {
-  bucket = "devops-static-assets-${random_id.bucket_suffix.hex}"
-  acl    = "private"
-}
-
-resource "random_id" "bucket_suffix" {
-  byte_length = 4
-}
