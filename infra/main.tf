5bd252775192a6ed4eebe01db2a7bff75bcb6694
kishore1307 <67825033+kishore1307@users.noreply.github.com>
2025-07-19 21:19:14 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -57,7 +57,7 @@ resource "aws_security_group" "web_sg" {
 }
 
 resource "aws_instance" "web" {
-  ami                         = "ami-0c02fb55956c7d316"
+  ami                         = "ami-0c94855ba95c71c99"
   instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.public.id
   vpc_security_group_ids      = [aws_security_group.web_sg.id]
